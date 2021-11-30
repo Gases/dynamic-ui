07f0a8394197ea2bd20c114809ddb2d7d53b1193
Alejandro <aariasmartijena@gmail.com>
2021-11-30 12:01:28 -0400

    finish image slider

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-const menuDropDown = document.querySelector('.menu-drop-down');
-const dropDownItems = document.querySelector('.drop-down-items');
-const classes = dropDownItems.classList;
+(function dropDownMenu() {
+  const menu = document.querySelector('.drop-down-menu');
+  const dropDownItems = document.querySelector('.drop-down-items');
+  const classes = dropDownItems.classList;
 
-menuDropDown.addEventListener('mouseleave', () => {
-  const result = classes.toggle('show');
+  menu.addEventListener('mouseleave', () => {
+    const result = classes.toggle('show');
 
-  if(!result) dropDownItems.classList.add('hide');
-  else dropDownItems.classList.remove('hide');
-});
+    if (!result) dropDownItems.classList.add('hide');
+    else dropDownItems.classList.remove('hide');
+  });
 
-menuDropDown.addEventListener('mouseenter', () => {
-  const result = classes.toggle('hide');
+  menu.addEventListener('mouseenter', () => {
+    const result = classes.toggle('hide');
 
-  if(!result) dropDownItems.classList.add('show');
-  else dropDownItems.classList.remove('show');
-});
+    if (!result) dropDownItems.classList.add('show');
+    else dropDownItems.classList.remove('show');
+  });
+})();
+
+(function imageSlider() {
+  let index = 1;
+  const images = document.querySelectorAll('.images');
+
+  const displayImage = (index) => {
+    images.forEach((image) => {
+      if(image.id == `image-${index}`) {
+        image.classList.add('show-image');
+        image.classList.remove('hide-image');
+      } else {
+        image.classList.add('hide-image');
+        image.classList.remove('show-image');
+      }
+    })
+  };
+  
+  const arrowButtons = document.querySelectorAll('.slider-button');
+  arrowButtons.forEach((direction) => direction.addEventListener('click', function() {
+      if (direction.id === 'left' && index > 1) index--;
+      else if (direction.id ==='right' && index < (images.length)) index++;
+
+      displayImage(index);
+    })
+  );
+
+  displayImage(index);
+})();
